73829650c5c93b87b3b73cfa88e3e74872490286
Philippe Hardardt <phil.henri@gmail.com>
2017-02-05 12:44:27 -0200

    Shorten test labels by removing the "label" word.

--- plotter_test.rb
+++ plotter_test.rb
@@ -5,165 +5,165 @@ require_relative 'plotter'
 
 class PlotterTest < Minitest::Test
   def test_plot_single_item
-    item = { label: 'Item label' }
+    item = { label: 'Item' }
 
-    assert_equal "Item label\n", Plotter.plot_tree(item)
+    assert_equal "Item\n", Plotter.plot_tree(item)
   end
 
   def test_plot_single_child
     tree = {
-      label: 'Parent label',
-      children: [{ label: 'Child label' }]
+      label: 'Parent',
+      children: [{ label: 'Child' }]
     }
 
     plot_result = <<~TREE
-      Parent label
-      └─ Child label
+      Parent
+      └─ Child
     TREE
     assert_equal plot_result, Plotter.plot_tree(tree)
   end
 
   def test_plot_multiple_children
     tree = {
-      label: 'Parent label',
+      label: 'Parent',
       children: [
-        { label: 'Child label 1' },
-        { label: 'Child label 2' },
-        { label: 'Child label 3' }
+        { label: 'Child 1' },
+        { label: 'Child 2' },
+        { label: 'Child 3' }
       ]
     }
 
     plot_result = <<~TREE
-      Parent label
-      ├─ Child label 1
-      ├─ Child label 2
-      └─ Child label 3
+      Parent
+      ├─ Child 1
+      ├─ Child 2
+      └─ Child 3
     TREE
     assert_equal plot_result, Plotter.plot_tree(tree)
   end
 
   def test_plot_granchild
-    granchild = { label: 'Granchild label' }
+    granchild = { label: 'Granchild' }
     tree = {
-      label: 'Parent label',
-      children: [{ label: 'Child label', children: [granchild] }]
+      label: 'Parent',
+      children: [{ label: 'Child', children: [granchild] }]
     }
 
     plot_result = <<~TREE
-      Parent label
-      └─ Child label
-         └─ Granchild label
+      Parent
+      └─ Child
+         └─ Granchild
     TREE
     assert_equal plot_result, Plotter.plot_tree(tree)
   end
 
   def test_plot_multiple_granchildren
     granchildren = [
-      { label: 'Granchild label 1' },
-      { label: 'Granchild label 2' },
-      { label: 'Granchild label 3' }
+      { label: 'Granchild 1' },
+      { label: 'Granchild 2' },
+      { label: 'Granchild 3' }
     ]
     tree = {
-      label: 'Parent label',
-      children: [{ label: 'Child label', children: granchildren }]
+      label: 'Parent',
+      children: [{ label: 'Child', children: granchildren }]
     }
 
     plot_result = <<~TREE
-      Parent label
-      └─ Child label
-         ├─ Granchild label 1
-         ├─ Granchild label 2
-         └─ Granchild label 3
+      Parent
+      └─ Child
+         ├─ Granchild 1
+         ├─ Granchild 2
+         └─ Granchild 3
     TREE
     assert_equal plot_result, Plotter.plot_tree(tree)
   end
 
   def test_plot_multiple_children_and_granchildren
     granchildren = [
-      { label: 'Granchild label 1' },
-      { label: 'Granchild label 2' },
-      { label: 'Granchild label 3' }
+      { label: 'Granchild 1' },
+      { label: 'Granchild 2' },
+      { label: 'Granchild 3' }
     ]
     tree = {
-      label: 'Parent label',
+      label: 'Parent',
       children: [
-        { label: 'Child label 1', children: granchildren },
-        { label: 'Child label 2', children: granchildren },
+        { label: 'Child 1', children: granchildren },
+        { label: 'Child 2', children: granchildren },
       ]
     }
 
     plot_result = <<~TREE
-      Parent label
-      ├─ Child label 1
-      │  ├─ Granchild label 1
-      │  ├─ Granchild label 2
-      │  └─ Granchild label 3
-      └─ Child label 2
-         ├─ Granchild label 1
-         ├─ Granchild label 2
-         └─ Granchild label 3
+      Parent
+      ├─ Child 1
+      │  ├─ Granchild 1
+      │  ├─ Granchild 2
+      │  └─ Granchild 3
+      └─ Child 2
+         ├─ Granchild 1
+         ├─ Granchild 2
+         └─ Granchild 3
     TREE
     assert_equal plot_result, Plotter.plot_tree(tree)
   end
 
   def test_plot_connection_line_for_granchildren_when_last_child
     grangranchildren = [
-      { label: 'Grangranchild label 1' },
-      { label: 'Grangranchild label 2' }
+      { label: 'Grangranchild 1' },
+      { label: 'Grangranchild 2' }
     ]
     granchildren = [
-      { label: 'Granchild label 1', children: grangranchildren },
-      { label: 'Granchild label 2' },
-      { label: 'Granchild label 3' }
+      { label: 'Granchild 1', children: grangranchildren },
+      { label: 'Granchild 2' },
+      { label: 'Granchild 3' }
     ]
     tree = {
-      label: 'Parent label',
+      label: 'Parent',
       children: [
-        { label: 'Child label 1', children: granchildren },
-        { label: 'Child label 2', children: granchildren }
+        { label: 'Child 1', children: granchildren },
+        { label: 'Child 2', children: granchildren }
       ]
     }
 
     plot_result = <<~TREE
-      Parent label
-      ├─ Child label 1
-      │  ├─ Granchild label 1
-      │  │  ├─ Grangranchild label 1
-      │  │  └─ Grangranchild label 2
-      │  ├─ Granchild label 2
-      │  └─ Granchild label 3
-      └─ Child label 2
-         ├─ Granchild label 1
-         │  ├─ Grangranchild label 1
-         │  └─ Grangranchild label 2
-         ├─ Granchild label 2
-         └─ Granchild label 3
+      Parent
+      ├─ Child 1
+      │  ├─ Granchild 1
+      │  │  ├─ Grangranchild 1
+      │  │  └─ Grangranchild 2
+      │  ├─ Granchild 2
+      │  └─ Granchild 3
+      └─ Child 2
+         ├─ Granchild 1
+         │  ├─ Grangranchild 1
+         │  └─ Grangranchild 2
+         ├─ Granchild 2
+         └─ Granchild 3
     TREE
     assert_equal plot_result, Plotter.plot_tree(tree)
   end
 
   def test_plot_single_child_with_nil_granchildren
     tree = {
-      label: 'Parent label',
-      children: [{ label: 'Child label', children: nil }]
+      label: 'Parent',
+      children: [{ label: 'Child', children: nil }]
     }
 
     plot_result = <<~TREE
-      Parent label
-      └─ Child label
+      Parent
+      └─ Child
     TREE
     assert_equal plot_result, Plotter.plot_tree(tree)
   end
 
   def test_plot_single_child_with_no_granchildren_node
     tree = {
-      label: 'Parent label',
-      children: [{ label: 'Child label' }]
+      label: 'Parent',
+      children: [{ label: 'Child' }]
     }
 
     plot_result = <<~TREE
-      Parent label
-      └─ Child label
+      Parent
+      └─ Child
     TREE
     assert_equal plot_result, Plotter.plot_tree(tree)
   end
